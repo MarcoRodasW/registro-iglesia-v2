379025855ebf87b272137533df408179f6c81b7c
MarcoRodasW <mrodasw@gmail.com>
2026-02-12 00:39:10 -0600

    chore: update metadata and favicons

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -40,10 +40,36 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 				content: "width=device-width, initial-scale=1",
 			},
 			{
-				title: "TanStack Start Starter",
+				title: "Registro de nuevos miembros - Casa de David",
 			},
 		],
 		links: [
+			{
+				rel: "icon",
+				href: "/favicon.ico",
+				sizes: "any",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "16x16",
+				href: "/favicon-16x16.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "32x32",
+				href: "/favicon-32x32.png",
+			},
+			{
+				rel: "apple-touch-icon",
+				sizes: "180x180",
+				href: "/apple-touch-icon.png",
+			},
+			{
+				rel: "manifest",
+				href: "/manifest.json",
+			},
 			{
 				rel: "preconnect",
 				href: "https://fonts.googleapis.com",
